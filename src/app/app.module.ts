0da0e9347cfa541f5a37b36949601dc0b4de61be
nermine.baatout <nermine.baatout@proxym-it.com>
2025-05-08 21:00:49 +0100

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { SignupComponent } from './signup/signup.component';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ProducteditComponent } from './product-edit/product-edit.component';  
+import { NzNotificationModule } from 'ng-zorro-antd/notification';
 import { ProductService } from './services/product.service';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { authInterceptorProviders } from './auth.interceptor'; // ✅ Interceptor provider
@@ -31,6 +32,7 @@ import { authInterceptorProviders } from './auth.interceptor'; // ✅ Intercepto
     ReactiveFormsModule,
     HttpClientModule,
     BrowserAnimationsModule,
+    NzNotificationModule
   ],
   providers: [
     provideClientHydration(),
